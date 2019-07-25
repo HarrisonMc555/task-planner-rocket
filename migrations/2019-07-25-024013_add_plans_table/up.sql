276c06d917db64f55ca589e52e9c30851543fd16
Harrison McCullough <mccullough.harrison@gmail.com>
2019-07-24 21:21:45 -0700

    Rename and fix migrations
    Use the `diesel migration` naming convention.
    
    The "up" migration wasn't working since, apparently, in sqlite you have to put
    the foreign key constraint at the end.

--- migrations/2019-07-25-024013_add_plans_table/up.sql
+++ migrations/2019-07-25-024013_add_plans_table/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE plans (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     taskid INTEGER NOT NULL,
-    FOREIGN KEY(taskid) REFERENCES tasks(id),
     description VARCHAR,
-    completed BOOLEAN NOT NULL DEFAULT 0
+    completed BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY(taskid) REFERENCES tasks(id)
 );
 
