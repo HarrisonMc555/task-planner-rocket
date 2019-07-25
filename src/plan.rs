94bab59df754aee88677466227acc37db9ced09a
Harrison McCullough <mccullough.harrison@gmail.com>
2019-07-25 09:09:11 -0700

    Rename from Item and ItemWithId to InsertableItem and Item

--- src/plan.rs
+++ src/plan.rs
@@ -1,5 +1,4 @@
-use diesel;
-use diesel::prelude::*;
+use diesel::{self, prelude::*};
 
 mod schema {
     table! {
@@ -14,30 +13,29 @@ mod schema {
 
 use self::schema::plans;
 use self::schema::plans::dsl::plans as all_plans;
-use crate::task::TaskWithId;
+use crate::task::Task;
 
-#[derive(Serialize, Identifiable, Queryable, Associations, Debug, Clone)]
-#[belongs_to(TaskWithId, foreign_key = "task_id")]
-#[table_name = "plans"]
-pub struct PlanWithId {
+#[derive(Identifiable, Queryable, AsChangeset, Associations, Serialize, Deserialize)]
+#[belongs_to(Task, foreign_key = "task_id")]
+pub struct Plan {
     pub id: i32,
     pub task_id: i32,
     pub description: Option<String>,
     pub completed: bool,
 }
 
-#[derive(Serialize, Insertable, Associations, Debug, Clone)]
+#[derive(Serialize, Insertable, Debug, Clone)]
 #[table_name = "plans"]
-pub struct Plan {
+pub struct InsertablePlan {
     pub description: Option<String>,
     pub completed: bool,
 }
 
-impl PlanWithId {
-    pub fn all(conn: &SqliteConnection) -> Vec<PlanWithId> {
+impl Plan {
+    pub fn all(conn: &SqliteConnection) -> Vec<Plan> {
         all_plans
             .order(plans::id.desc())
-            .load::<PlanWithId>(conn)
+            .load::<Plan>(conn)
             .unwrap()
     }
 }
